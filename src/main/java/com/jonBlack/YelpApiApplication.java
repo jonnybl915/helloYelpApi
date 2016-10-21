e685771a1fe7f4776aef8fa0d2ca0a1cc441271c
jonathandavidblack <jonnybl915@gmail.com>
2016-10-21 11:37:58 -0400

    initial import

---
+++ src/main/java/com/jonBlack/YelpApiApplication.java
@@ -0,0 +1,12 @@
+package com.jonBlack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpApiApplication.class, args);
+	}
+}
